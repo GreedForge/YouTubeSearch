486267394a95deb84d589c1e3e8f02741eb7433a
GreedForge <jafecafe@gmail.com>
2017-09-25 13:33:53 -0500

    added playable video embed, video details, video list

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+	// const video = props.video; above curly brace video is the same as this line ES6
+
+	const imageUrl = video.snippet.thumbnails.default.url;
+
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}</div>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAYP7QuDpR_QO2uTruZA9BCgUTPEIqDXms';
 
 
@@ -23,6 +24,7 @@ class App extends Component {
 		return(
 	 	<div>
 	 		<SearchBar />
+	 		<VideoDetail video={this.state.videos[0]} />
 	 		<VideoList videos={this.state.videos} />
 	 	</div>
 		 );
