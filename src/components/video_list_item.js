3fb3f7101f026f05f457df80d8c88ca63e772fed
GreedForge <jafecafe@gmail.com>
2017-09-26 14:38:38 -0500

    a few more additions to video list and we now have clickable videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
 	// const video = props.video; above curly brace video is the same as this line ES6
 
 	const imageUrl = video.snippet.thumbnails.default.url;
 
 	return (
-		<li className="list-group-item">
+		<li onClick={() => onVideoSelect(video)} className="list-group-item">
 			<div className="video-list media">
 				<div className="media-left">
 					<img className="media-object" src={imageUrl} />
