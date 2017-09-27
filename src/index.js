bdbde57f72b9085f43bd1bed6cc6a0c8deeaf300
GreedForge <jafecafe@gmail.com>
2017-09-26 21:55:43 -0500

    finished the initial styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -18,7 +19,12 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');
+		
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
@@ -26,10 +32,13 @@ class App extends Component {
 				// the above resolves to this.setState({ videos: videos});
 		});
 	}
+
 	render(){
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
 		return(
 	 	<div>
-	 		<SearchBar />
+	 		<SearchBar onSearchTermChange={videoSearch} />
 	 		<VideoDetail video={this.state.selectedVideo} />
 	 		<VideoList
 	 		onVideoSelect={selectedVideo => this.setState({selectedVideo})}
